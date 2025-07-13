e26d63a3c38999f8c3b115eeadb2db1bc061c1c1
shivangi-2005-git <sonishivangi001@gmail.com>
2025-07-13 20:31:30 +0530

    sum of 2 number

---
+++ Add 2 num.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+    // sum two number
+   int sum(int a, int b){
+  int s= a+b;
+  return s;
+   }
+   int main(){
+    cout<<10+5<<endl;
+return 0;
+   }
