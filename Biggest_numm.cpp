323200d71a7dd2aef1d66aef58639e441edc0f8f
shivangi-2005-git <sonishivangi001@gmail.com>
2025-07-15 22:22:12 +0530

    Print largest number

---
+++ Biggest_numm.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int a,b;
+    cout<<"Enter two number : " ;
+    cin>>a>>b;
+    if (a>b)
+    
+    cout<<"Largest number= "  <<a;
+    else
+    cout<<"largest number = "<<b;
+
+return 0;
+}
