190260c88e3e3729bb5e00f405010685675cb722
shivangi-2005-git <sonishivangi001@gmail.com>
2025-07-16 22:31:25 +0530

    sum of digit of array

---
+++ sum_of_array.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+
+    cout << "Sum of elements = " << sum << endl;
+    return 0;
+}
