2e066a076e14082d8dc1131227234617f0725852
shivangi-2005-git <sonishivangi001@gmail.com>
2026-06-09 14:11:39 +0530

    Time: 0 ms (100%), Space: 13.3 MB (35.73%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        d = {}
+
+        for i in range(len(nums)):
+            complement = target - nums[i]
+
+            if complement in d:
+                return [d[complement], i]
+
+            d[nums[i]] = i
